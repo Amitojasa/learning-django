a2f2ca87eda36723df7cdf2cec74c161e6b3179a
Amitojasa <amitojvmc@gmail.com>
2019-12-18 18:13:50 +0530

    Work under progress

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render ,redirect
 from django.contrib.auth.models import User,auth
-
+from django.contrib import messages
 # Create your views here.
 
 
@@ -12,22 +12,43 @@ def register(request):
         username=request.POST['userName']
         password=request.POST['password']
         confirmPassword=request.POST['confirmPassword']
-        if password== confirmPassword:   
+        if password== confirmPassword: 
+
             if User.objects.filter(username=username).exists():
-                print("cant take this username")
-                return redirect("/")   
+                messages.info(request,"UserName taken")
+                return redirect('register')
             elif User.objects.filter(email=email).exists():
-                print("cant take this email id already taken")
-                return redirect("/")   
+                messages.info(request,"email taken")
+                return redirect('register') 
             else:
                 user=User.objects.create_user(username=username,password=password,email=email,first_name=firstName,last_name=lastName)
                 user.save()
                 print("user created")
+                return redirect("login")
         else:
-            print("password dont match")
-            return redirect("/")
+            messages.info(request,"password dont match")
+            return redirect('register')
 
 
     else:
         return render(request,'register.html')
 
+
+def login(request):
+    if request.method=='POST':
+        username= request.POST['username']
+        password=request.POST['password']
+
+        user= auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect("/")
+        else:
+            messages.info(request,"Invalid details")
+            return redirect("login")
+    else:
+        return render(request,'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect("/")
